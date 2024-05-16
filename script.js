59b46de1da0cd692dd8c111475ac2f3ec668f1f1
matifelitti <matiasfelitti@gmail.com>
2024-05-16 19:36:41 -0300

    html and js brands code, brands images

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const brandsLinks = document.getElementById("brands-links");
+  const brandsContainer = document.getElementById("brands-container");
+
+  brandsLinks.addEventListener("click", (event) => {
+    event.preventDefault();
+    brandsContainer.scrollIntoView({ behavior: "smooth" });
+  });
+
   const magazineLink = document.getElementById("magazine-link");
   const magazineContainer = document.getElementById("magazine-container");
 
