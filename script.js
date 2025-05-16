53a7054ca944de74dd93ed7ed505837525e05b30
matifelitti <matiasfelitti@gmail.com>
2025-05-16 20:12:00 -0300

    scroll fade and carousel transition

--- script.js
+++ script.js
@@ -47,4 +47,27 @@ document.addEventListener("DOMContentLoaded", function () {
       cartItem.appendChild(newItem);
     });
   });
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  document.querySelectorAll(".scroll-fade").forEach((el) => {
+    observer.observe(el);
+  });
+
+  const year = new Date().getFullYear();
+  document.getElementById(
+    "year"
+  ).innerHTML = `&copy; ${year} Clothes.Inc. All rights reserved.`;
 });
